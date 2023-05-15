98d7b32861860ca4fb872336c6b12a0819bcba70
Matěj <matej.nos@email.cz>
2023-05-16 00:32:04 +0200

    vyres a vytvor typy jako metody, class student

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from xml.dom import minidom
 import random
 from sympy import *
-
-def vytvor_typy_pr(priklady):
-
-    typy_prikladu = {}
-
-    for priklad in priklady:
-        if priklad.getAttribute("id") not in typy_prikladu.keys():
-            typy_prikladu[priklad.getAttribute("id")] = [priklad]
-        else:
-            typy_prikladu[priklad.getAttribute("id")].append(priklad)
-
-    return typy_prikladu
+import pickle
 
 
 def vytvor_seznam_pr(typy_pr, pozadovane_typy):     #TODO spatny format?
@@ -41,34 +30,10 @@ def vytvor_komplet_priklad(priklad):        #TODO zlomky, floaty, -,  kontrola m
     return Priklad(zadani)
 
 
-def vyres_priklad(priklad):     #TODO Nerovnosti a výrazy
-    
-    if ('=' in priklad) and ('<' not in priklad and '>' not in priklad):
-
-        rovnitko = priklad.find('=')
-        left_side = priklad[:rovnitko]
-        left_side = left_side.replace('x', '*x')
-        left_side = sympify(left_side)
-
-        right_side = priklad[rovnitko + 1:]
-        right_side = right_side.replace('x', '*x')
-        right_side = sympify(right_side)
-
-        reseni = solve(Eq(left_side, right_side))
-
-        return reseni
-
-    elif '<' in priklad or '>' in priklad:
-        ...
-    else:
-        ...
-
-
 class Database:         #TODO Špatná cesta, změna cesty?
     def __init__(self, path):
 
         self._document = minidom.parse(path)
-        self._typy_prikladu = vytvor_typy_pr(self._document.getElementsByTagName('priklad'))
 
     @property
     def document(self):
@@ -77,19 +42,23 @@ class Database:         #TODO Špatná cesta, změna cesty?
     def document(self, value):
         raise AttributeError("Nemůžete změnit hodnotu atributu.")
     
-    @property
-    def typy_prikladu(self):
-        return self._typy_prikladu
-    @typy_prikladu.setter
-    def typy_prikladu(self, value):
-        raise AttributeError("Nemůžete změnit hodnotu atributu.")
+    def vytvor_typy_pr(self):
+
+        typy_prikladu = {}
+
+        for priklad in self._document.getElementsByTagName('priklad'):
+            if priklad.getAttribute("id") not in typy_prikladu.keys():
+                typy_prikladu[priklad.getAttribute("id")] = [priklad]
+            else:
+                typy_prikladu[priklad.getAttribute("id")].append(priklad)
+
+        return typy_prikladu
     
 
 class Priklad:
 
     def __init__(self, priklad):
         self._priklad = priklad
-        self._reseni = vyres_priklad(priklad)
         self._uspesnost = []
     
     @property
@@ -100,24 +69,62 @@ class Priklad:
         raise AttributeError("Nemůžete změnit hodnotu atributu.")
     
     @property
-    def reseni(self):
-        return self._reseni
-    @reseni.setter
-    def reseni(self, value):
-        raise AttributeError("Nemůžete změnit hodnotu atributu.")
-    
-    @property
     def uspesnost(self):
         return sum(self._uspesnost)/len(self._uspesnost)
     @uspesnost.setter
     def uspesnost(self, value):
         self._uspesnost.append(value)
 
+    def vyres_priklad(self):     #TODO Nerovnosti a výrazy
+
+        if ('=' in self._priklad) and ('<' not in self._priklad and '>' not in self._priklad):
+
+            rovnitko = self._priklad.find('=')
+            left_side = self._priklad[:rovnitko]
+            left_side = left_side.replace('x', '*x')
+            left_side = sympify(left_side)
+
+            right_side = self._priklad[rovnitko + 1:]
+            right_side = right_side.replace('x', '*x')
+            right_side = sympify(right_side)
+
+            reseni = solve(Eq(left_side, right_side))
+
+            return reseni
+
+        elif '<' in self._priklad or '>' in self._priklad:
+            ...
+        else:
+            ...
+
     
 class Student:
 
-    def __init__():
-        ...
+    def __init__(self, jmeno):
+        self._jmeno = jmeno
+        self._znamky = []
+
+    @property
+    def jmeno(self):
+        return self._jmeno
+    @jmeno.setter
+    def jmeno(self, value):
+        self._jmeno = value
+
+    @property
+    def znamky(self):
+        return f'Tvoje znamky jsou: {self._znamky} a tvůj průměr je: {sum(self._znamky)/len(self._znamky)}.'
+    @znamky.setter
+    def trida(self, value):
+        raise AttributeError("Nemůžete změnit hodnotu atributu.")
+    
+    def pridat_znamky(self, znamka):
+        self._znamky.append(znamka)
+
+    def ulozit(self, slozka):
+        soubor = f'studenti/{self._jmeno}.pkl'
+        with open(soubor, 'wb') as file:
+            pickle.dump(self, file)
 
 
 def main():         #TODO ofc dodelat a prehlednejsi?
@@ -131,7 +138,7 @@ def main():         #TODO ofc dodelat a prehlednejsi?
     soubor = Database(path)
 
     seznam_pr = []
-    for priklad in vytvor_seznam_pr(soubor.typy_prikladu, typy):
+    for priklad in vytvor_seznam_pr(soubor.vytvor_typy_pr(), typy):
 
         seznam_pr.append(vytvor_komplet_priklad(priklad))
 
@@ -140,11 +147,11 @@ def main():         #TODO ofc dodelat a prehlednejsi?
         print(zadani.priklad)
         odpoved = sympify(input('Výsledek?'))
         
-        if odpoved == zadani.reseni[0]:
-            print(f'GJ vysledek je {zadani.reseni[0]}')
+        if odpoved == zadani.vyres_priklad()[0]:
+            print(f'GJ vysledek je {zadani.vyres_priklad()[0]}')
             zadani.uspesnost = 1
         else:
-            print(f'bad, reseni je {zadani.reseni[0]}')
+            print(f'bad, reseni je {zadani.vyres_priklad()[0]}')
             zadani.uspesnost = 0
 
     for zadani in seznam_pr:
